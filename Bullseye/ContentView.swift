6cdf76a8d81926b2508c8529c6514475c9e1c643
Damien Sedgwick <damiensedgwick@icloud.com>
2021-10-05 17:30:58 +0100

    Built basic layout and set min iOS version to iOS 15.

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    VStack {
+      Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN")
+      Text("89")
+      HStack {
+        Text("1")
+        Slider(value: .constant(50), in: 1.0...100.0)
+        Text("100")
+      }
+      Button(action: {}) {
+        Text("Hit me")
+      }
+    }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+      .previewInterfaceOrientation(.landscapeRight)
   }
 }
