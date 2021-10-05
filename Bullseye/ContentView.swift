d82d85b3d6e1f4e6d5a49c6eab5da83b39815d1c
Damien Sedgwick <damiensedgwick@icloud.com>
2021-10-05 19:09:13 +0100

    WIP: adding styles to UI

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,15 +10,28 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     VStack {
-      Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN")
+      Text("PLACE THE BULLSEYE AS CLOSE AS YOU CAN")
+        .font(.footnote)
+        .kerning(2.0)
+        .bold()
+        .lineSpacing(5)
+        .multilineTextAlignment(.center)
+      
       Text("89")
+        .font(.title)
+        .bold()
+      
       HStack {
         Text("1")
-        Slider(value: .constant(50), in: 1.0...100.0)
+          .bold()
+        Slider(value: .constant(32), in: 1.0...100.0)
         Text("100")
+          .bold()
       }
+      
       Button(action: {}) {
         Text("Hit me")
+          .bold()
       }
     }
   }
