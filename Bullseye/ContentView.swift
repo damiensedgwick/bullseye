223038c56cb250f22e4f32f366127b1e5b9c1921
Damien Sedgwick <damiensedgwick@icloud.com>
2021-10-07 20:57:36 +0100

    Building out app functionality and adding in state values.

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  @State private var isAlertVisible: Bool = false
+  @State private var targetValue: Double = Double.random(in: 1.0...100.0)
+  @State private var sliderValue: Double = 50.0
+  
   var body: some View {
     VStack {
       Text("PLACE THE BULLSEYE AS CLOSE AS YOU CAN")
@@ -18,22 +23,34 @@ struct ContentView: View {
         .multilineTextAlignment(.center)
       
       Text("89")
-        .font(.title)
-        .bold()
+        .font(.largeTitle)
+        .fontWeight(.black)
+        .kerning(-1.0)
       
       HStack {
         Text("1")
           .bold()
-        Slider(value: .constant(32), in: 1.0...100.0)
+        
+        Slider(value: self.$sliderValue, in: 1.0...100.0)
+        
         Text("100")
           .bold()
       }
       
-      Button(action: {}) {
+      Button(action: {
+        self.isAlertVisible = true
+      }) {
         Text("Hit me")
           .bold()
       }
+      .alert(isPresented: $isAlertVisible, content: {
+        
+        let roundedValue = Int(self.sliderValue.rounded())
+        
+        return Alert(title: Text("Slider Value"), message: Text("\(roundedValue)"), dismissButton: .default(Text("Awesome")))
+      })
     }
+    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
   }
 }
 
