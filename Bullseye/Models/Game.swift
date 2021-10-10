bd068e388b9f2eb41c6666caed9e53b6926ccebd
Damien Sedgwick <damiensedgwick@icloud.com>
2021-10-10 19:14:56 +0100

    WIP: Building out app using TDD. Tests currently failing.

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Game {
-  var targetValue: Int = 42
+  var targetValue: Int = Int.random(in: 1...100)
   var totalScore: Int = 0
   var currentRound: Int = 1
   
---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,36 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Damien K. Sedgwick on 10/10/2021.
+//
+
+import XCTest
+@testable import Bullseye
+
+class BullseyeTests: XCTestCase {
+  
+  var game: Game!
+  
+  override func setUpWithError() throws {
+    game = Game()
+  }
+  
+  override func tearDownWithError() throws {
+    game = nil
+  }
+  
+  func testScorePositive() {
+    let guess = game.targetValue + 5
+    let score = game.points(sliderValue: guess)
+    
+    XCTAssertEqual(score, 95)
+  }
+  
+  func testScoreNegative() {
+    let guess = game.targetValue - 5
+    let score = game.points(sliderValue: guess)
+    
+    XCTAssertEqual(score, 95)
+  }
+}
